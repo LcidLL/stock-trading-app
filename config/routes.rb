02e908a1041a0c873f44e36e33d4c99484c9d860
Lcid Lumenario <lumenariolcid@gmail.com>
2025-07-17 19:36:54 +0800

    installed devise and added migration files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get "pages/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+
   root "pages#index"
   get '/dashboard', to: "pages#index"
 end
