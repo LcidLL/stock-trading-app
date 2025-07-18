fd1fa09fd86024e4c3352e647185d4a63cf86fc4
Jocer <jocerxnew@gmail.com>
2025-07-18 20:37:51 +0800

    created model, views and controller for login and for admin

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  before_action :authenticate_admin_user!, except: [:index]
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "pages/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  ActiveAdmin.routes(self)
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -12,5 +16,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
-  get '/dashboard', to: "pages#index"
+  #get '/dashboard', to: "pages#index"
+  get 'login', to: 'login#new'
+  post 'login', to: 'login#create'
 end
